304f068d54cee33e7d37ad643181843c75070be8
Alex Luo <alexbluo01@gmail.com>
2022-07-07 15:33:33 -0400

    initial commit:

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+import { }
+import { Box } from "@chakra-ui/react";
+
+const ChakraBox = 
+
+const Home: NextPage = () => {
+  return <Box bg="black">text</Box>;
+};
+
+export default Home;

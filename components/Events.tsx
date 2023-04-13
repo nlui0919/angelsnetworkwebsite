b659dd80fb9f68ca67a203fe1edb1c526c943b53
Nathan Lui <nathanlui@Nathans-MacBook-Air.local>
2023-04-13 09:00:14 -0400

    April 13th changes

--- components/Events.tsx
+++ components/Events.tsx
@@ -6,19 +6,54 @@ export default function Events() {
   return (
     <div className="bg-[#f5eddd] px-0 pb-16 w-full overflow-hidden">
     
-        <h2 className="text-center text-8xl text-gray-800 font-bold"> Events </h2>
+        <h2 className="text-center text-8xl text-gray-800 font-bold mt-6"> Events </h2>
 
-        <div className=" xl:flex-row flex flex-col xl:pt-16 pt-6"> {/** each event */}
-          <div className="px-6 xl:pr-32 flex flex-col xl:gap-y-3 gap-y-6"> 
-            <div className="xl:ml-[20rem] xl:pt-4 xl:text-left text-center items-center text-gray-800 text-5xl xl:font-bold font-medium">October: Breast Cancer Awareness Tie-Dying</div>
-            <div className="xl:ml-[20rem] xl:pt-4 xl:text-left text-center items-center text-gray-700 text-lg"> Angel's network tie-dyes shirts. All proceceds are donated to breat cancer research. Also alex sucks. Also alex sucks. Also alex sucks. Also alex sucks. Also alex sucks.  </div>
-          </div>
-          <div className=" xl:pr-64 xl:mr-16 xl:ml-0 xl:pt-0 pt-6 mx-auto">
-            <div className="bg-gradient-to-r from-pink-300 via-red-300 to-purple-400 rounded-3xl h-[23rem] w-[30rem] xl:h-[33rem] xl:w-[43rem] bg-gradient-to-br">
-              <div className="xl:p-6 p-4"><Image src={BCA} className="grow shrink basis-0 rounded-3xl object-fill"></Image></div>
+        <div className= "xl:flex-row flex flex-col justify-center xl:pt-16 pt-6"> {/** each event */}
+          {/* <div className = "xl:flex-row flex-col justify-center"> */}
+            <div className="px-6 xl:w-3/12 flex flex-col xl:gap-y-3 gap-y-6"> 
+              <div className="xl:pt-4 xl:text-left text-center items-center text-gray-800 text-5xl xl:font-bold font-medium">October: Breast Cancer Awareness Tie-Dying</div>
+              <div className="xl:pt-4 xl:text-left xl:pr-0 xl:pl-0 text-center md:pl-32 md:pr-32 items-center text-gray-700 text-lg"> Tie-dyes shirts pink for breast cancer awareness! Create pink breast cancer awareness symbols for each class to hang up in their room. Shirts are sold at lunch the following week. All proceceds are donated to breast cancer research.  </div>
             </div>
-          </div>
-        </div>    
+            <div className="xl:ml-0 xl:pt-0 pt-6 pr-6 pl-6">
+              <div className="mx-auto w-fit from-pink-300 xl:p-6 p-4 via-red-300 to-purple-400 rounded-3xl bg-gradient-to-br">
+                <Image src={BCA} className="rounded-2xl h-full" objectFit='cover'></Image>  
+              </div>
+            </div>
+          {/* </div> */}
+        </div>  
+
+
+        <div className= "xl:flex-row flex flex-col-reverse justify-center xl:pt-16 pt-6"> {/** each event */}
+          {/* <div className = "xl:flex-row flex-col justify-center"> */}
+            <div className="xl:ml-0 xl:pt-0 pt-6 pr-6 pl-6">
+              <div className="mx-auto w-fit from-red-300 xl:p-6 p-4 via-purple-400 to-pink-300 rounded-3xl bg-gradient-to-br">
+                <Image src={BCA} className="rounded-2xl h-full" objectFit='cover'></Image>  
+              </div>
+            </div>
+            <div className="px-6 xl:w-3/12 flex flex-col xl:gap-y-3 gap-y-6"> 
+              <div className="xl:pt-4 xl:text-left text-center items-center text-gray-800 text-5xl xl:font-bold font-medium"> November: Grassroots Lunch Packing</div>
+              <div className="xl:pt-4 xl:text-left xl:pr-0 xl:pl-0 text-center md:pl-32 md:pr-32 items-center text-gray-700 text-lg"> Donate and pack lunches/snacks to donate to grassroots! Draw and write encouraging cards for any recipients. Any potential snack donations such as chips, applesauce, juice, water bottles, et cetera are greatly appreciated! </div>
+            </div>
+          {/* </div> */}
+        </div>  
+
+        <div className= "xl:flex-row flex flex-col justify-center xl:pt-16 pt-6"> {/** each event */}
+          {/* <div className = "xl:flex-row flex-col justify-center"> */}
+            <div className="px-6 xl:w-3/12 flex flex-col xl:gap-y-3 gap-y-6"> 
+              <div className="xl:pt-4 xl:text-left text-center items-center text-gray-800 text-5xl xl:font-bold font-medium"> December: Candymarks Bookmark Creation</div>
+              <div className="xl:pt-4 xl:text-left xl:pr-0 xl:pl-0 text-center md:pl-32 md:pr-32 items-center text-gray-700 text-lg"> Cutout, draw, and laminate holiday themed bookmarks. Attatch candy canes to each bookmark. All bookamrks are donated to local hospitals.  </div>
+            </div>
+            <div className="xl:ml-0 xl:pt-0 pt-6 pr-6 pl-6">
+              <div className="mx-auto w-fit from-pink-300 xl:p-6 p-4 via-red-300 to-purple-400 rounded-3xl bg-gradient-to-br">
+                <Image src={BCA} className="rounded-2xl h-full" objectFit='cover'></Image>  
+              </div>
+            </div>
+          {/* </div> */}
+        </div>  
+
+        
+
+        
     
     </div>
   )
--- components/Title.tsx
+++ components/Title.tsx
@@ -1,8 +1,8 @@
 export default function Title() {
     return (
       <>
-        <div className = "bg-gradient-to-bl from-purple-500 to-pink-500 text-white w-screen h-screen flex justify-center items-center">
-            <h1 className = "text-9xl font-bold"> Angel's Network </h1>
+        <div className = "bg-gradient-to-bl from-pink-300 via-red-300 text-white w-screen h-screen flex justify-center items-center">
+            <h1 className = "text-9xl font-bold text-center"> Angel's Network </h1>
         </div>
       </>
     );
